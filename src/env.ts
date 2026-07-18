6d8abe52292a0c56e38cd63c30098284f5dfad0b
David Stenstrøm <david@stenstroem.dk>
2026-07-18 09:26:51 +0200

    feat: add base url of api to env

--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,7 @@ const envSchema = z.object({
   DEVICE_ID: z.string(),
   PUSHOVER_TOKEN: z.string(),
   PUSHOVER_USER: z.string(),
+  API_BASE_URL: z.url().default('https://home-api.smartdevice.liebherr.com/v1'),
 });
 
 export const env = envSchema.parse(process.env);
